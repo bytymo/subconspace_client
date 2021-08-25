feefe3bb0c3d68b5b28abd83b2f2ad8b704b3325
byronmosley <mosleyb91@outlook.com>
2021-08-24 17:38:05 -0700

    added landing page with calendly config. WIP questionnaire

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Home from './pages/home';
 import Navbar from './components/navbar';
 import Booking from './pages/booking';
 
-function App() {
+const App = () => {
   return (
     <Router>
       <div className='App'>
