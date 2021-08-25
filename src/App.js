433d05170deb9dca4376b22b432e163c75cbf7c9
byronmosley <mosleyb91@outlook.com>
2021-08-24 17:19:41 -0700

    added landing page with calendly config. WIP questionnaire

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import './App.css';
-import Home from './pages/home';
-import Navbar from './components/navbar';
-import Booking from './pages/booking';
+import Home from 'pages/home';
+import Navbar from 'components/navbar';
+import Booking from 'pages/booking';
 
 function App() {
   return (
