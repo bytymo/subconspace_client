a08e04294082b1a4c1f7b828adc46374b8cf50b9
byronmosley <mosleyb91@outlook.com>
2021-08-30 09:16:34 -0700

    responsiveness to divider to disappear on small and xs screens

--- src/components/divider.js
+++ src/components/divider.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Divider = () => {
   return (
-    <div className='divider'>
+    <div className='divider d-none d-md-flex' >
       <div className="box1">
         <div className="box2">
           <div className="box3"></div>
