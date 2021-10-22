85ded2806d6cc1d2daae6100da4d25e934dffacc
byronmosley <mosleyb91@outlook.com>
2021-10-22 10:03:30 -0700

    fixed for netlify build

--- src/components/header.js
+++ src/components/header.js
@@ -1,8 +1,8 @@
 import React from 'react';
-import black from '../images/lotus-black.png';
-import blue from '../images/lotus-blue.png';
-import dark_purple from '../images/lotus-purple-1.png';
-import light_purple from '../images/lotus-purple-2.png';
+// import black from '../images/lotus-black.png';
+// import blue from '../images/lotus-blue.png';
+// import dark_purple from '../images/lotus-purple-1.png';
+// import light_purple from '../images/lotus-purple-2.png';
 import white from '../images/lotus-white.png';
 
 const Header = () => {
