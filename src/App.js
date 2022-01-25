97f41a0c713ef57be850558a62d25edd4ee440ba
byronmosley <mosleyb91@outlook.com>
2022-01-25 16:24:38 -0700

    added book purchase page. removed thank you page link in booking.js. added book images.

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import './App.css';
 import Home from './pages/home';
 import Navbar from './components/navbar';
+import Book from './pages/book';
 import Booking from './pages/booking';
 import Thankyou from './pages/thankyou';
 import UserAgreement from './pages/user-agreement';
@@ -16,6 +17,7 @@ const App = () => {
           <Switch>
             <Route exact path='/agreement' component={UserAgreement} />
             <Route exact path='/booking' component={Booking} />
+            <Route exact path='/book' component={Book} />
             <Route exact path='/thankyou' component={Thankyou} />
             <Route exact path='/' component={Home} />
           </Switch>
