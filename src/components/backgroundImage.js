44cf324d9f1848979e89347fa8474a51e8f7c30b
byronmosley <mosleyb91@outlook.com>
2022-03-13 17:31:53 -0700

    fixed mobile and tablet view parallax image on scroll

--- src/components/backgroundImage.js
+++ src/components/backgroundImage.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import mobileImg from '../images/purple-sky-moon-square.jpg'
 
 const BackgroundImage = () => {
   return (
-    <div className="parallax">
-      
-    </div>
+    <>
+      <div className="parallax d-none d-lg-block" />
+      <div className="d-lg-none" >
+        <img src={mobileImg} width='100%' alt="mobile version of parallax moon"  />
+      </div>
+    </>
   );
 };
 
